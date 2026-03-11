911f80d275dd6fdf02b33633fc0a2a2c4e4460f9
zachrtermanini-cmyk <zachrtermanini@gmail.com>
2026-03-11 19:43:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -26643,7 +26643,7 @@ function BeeSwarmSimulator(DATA){
                 out.removeAirFrictionUntilGrounded=false
                 
                 if(user.keys[' ']||jumpingButton){
-                    out.grounded=true
+                    out.grounded=false
                     out.body.velocity.y=out.jumpPower
                 }
                 
@@ -34760,6 +34760,7 @@ function BeeSwarmSimulator(DATA){
     
 
 }
+
 
 
 
